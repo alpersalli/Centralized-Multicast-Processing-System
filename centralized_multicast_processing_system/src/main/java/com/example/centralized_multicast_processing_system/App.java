c34ef87e3eecce0bf2977b3616d6a9a9db13329d
Alper Şallı <alpersallidgb@gmail.com>
2026-01-18 04:37:12 +0300

    socket threads added.

--- centralized_multicast_processing_system/src/main/java/com/example/centralized_multicast_processing_system/App.java
+++ centralized_multicast_processing_system/src/main/java/com/example/centralized_multicast_processing_system/App.java
@@ -1,5 +1,8 @@
 package com.example.centralized_multicast_processing_system;
 
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
 import java.nio.ByteBuffer;
 
 /**
@@ -8,7 +11,7 @@ import java.nio.ByteBuffer;
  */
 public class App 
 {
-    public static void main( String[] args )
+    public static void main( String[] args ) throws IOException
     {
 
         PacketManager packetManager = new PacketManager();
@@ -22,5 +25,29 @@ public class App
         System.out.println(testPacket.getTrackNumber());
         System.out.println(testPacket.getPriority());
         System.out.println(testPacket.getPayload());
+
+        Thread serverThread = new Thread(()-> {
+            try {
+                ServerSocket serverSocket = new ServerSocket(6666);
+                Socket socketServer = serverSocket.accept();
+                System.out.println("Client accepted");
+            }
+            catch (IOException e){
+                e.printStackTrace();
+            }
+        }, "testServer");
+
+        Thread clientThread = new Thread(()-> {
+            try {
+                Socket socketClient = new Socket("127.0.0.1", 6666);
+                System.out.println("Client Connected");
+            }
+            catch (IOException e){
+                e.printStackTrace();
+            }
+        }, "testClient");
+
+        serverThread.start();
+        clientThread.start();
     }
 }
