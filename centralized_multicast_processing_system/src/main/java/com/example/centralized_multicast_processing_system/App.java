06e77544edcae5d88210f71283090225bea62a99
Alper Şallı <alpersallidgb@gmail.com>
2026-01-21 21:08:24 +0300

    TCP socket changed to UDP socket and multicast socket added.

--- centralized_multicast_processing_system/src/main/java/com/example/centralized_multicast_processing_system/App.java
+++ centralized_multicast_processing_system/src/main/java/com/example/centralized_multicast_processing_system/App.java
@@ -2,7 +2,10 @@ package com.example.centralized_multicast_processing_system;
 
 import java.io.FileInputStream;
 import java.io.IOException;
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
 import java.net.InetAddress;
+import java.net.MulticastSocket;
 import java.net.ServerSocket;
 import java.net.Socket;
 import java.nio.ByteBuffer;
@@ -23,8 +26,8 @@ public class App
         }
         String serverIp = props.getProperty("server.ip");
         int serverPort = Integer.parseInt(props.getProperty("server.port"));
-        String clientIp = props.getProperty("client.ip");
-        int clientPort = Integer.parseInt(props.getProperty("client.port"));
+        String multicastAddressIp = props.getProperty("multicastAddress.ip");
+        int multicastAddressPort = Integer.parseInt(props.getProperty("multicastAdrress.port"));
 
         PacketManager packetManager = new PacketManager();
         ByteBuffer testByteBuffer = packetManager.packet_to_byte(new PacketContent("10-12-2001",12,1,"TestData"));
@@ -40,23 +43,32 @@ public class App
 
         Thread serverThread = new Thread(() -> {
             try {
-                ServerSocket serverSocket = new ServerSocket(serverPort, 50, InetAddress.getByName(serverIp));
+                // ServerSocket serverSocket = new ServerSocket(serverPort, 50, InetAddress.getByName(serverIp));
+                DatagramSocket udpSocketServer = new DatagramSocket(serverPort, InetAddress.getByName(serverIp));
                 System.out.println("Client accepted");
                 PacketContent receivedPacketContent = new PacketContent();
                 byte[] buffer = new byte[1024];
 
                 while (true) {
-                    Socket socketServer = serverSocket.accept();
-
-                    int nextByte = socketServer.getInputStream().read(buffer);
-                    if (nextByte != -1) {
-                        Arrays.copyOf(buffer, nextByte);
-                        receivedPacketContent = packetManager.packet_to_String(buffer);
-                        System.out.println(receivedPacketContent.getTimestamp());
-                        System.out.println(receivedPacketContent.getTrackNumber());
-                        System.out.println(receivedPacketContent.getPriority());
-                        System.out.println(receivedPacketContent.getPayload());
+                    // Socket socketServer = serverSocket.accept();
+                    DatagramPacket packet = new DatagramPacket(buffer, buffer.length);
+                    udpSocketServer.receive(packet);
+                    receivedPacketContent = packetManager.packet_to_String(buffer);
+                    System.out.println(receivedPacketContent.getTimestamp());
+                    System.out.println(receivedPacketContent.getTrackNumber());
+                    System.out.println(receivedPacketContent.getPriority());
+                    System.out.println(receivedPacketContent.getPayload());
+                    if (buffer != null) {
+                        MulticastSocket multicastSocket = new MulticastSocket();
+                        InetAddress group = InetAddress.getByName(multicastAddressIp);
+                        DatagramPacket pxacket = new DatagramPacket(buffer, buffer.length, group, multicastAddressPort);
+                        for (int x = 0 ; x<5; x++){
+                            multicastSocket.send(pxacket);
+                        }
+                        multicastSocket.close();
+                        System.out.println("Multicast response send to all devices in same network!");
                     }
+                    
                 }
             } catch (IOException e) {
                 e.printStackTrace();
